08b20c12ff2068a014e8714f78c5e82e085cc579
aswinnarayanan <aswinn89@gmail.com>
2021-10-14 05:39:38 +0000

    Add extra files

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,4 +3,5 @@
 bash stop_and_clean.sh
 
 docker build -t $(whoami)/$(basename ${PWD}) .
-docker run -it --rm -p 8080:8080 -p 8888:8888 $(whoami)/$(basename ${PWD})
+docker run --rm -p 8080:8080 -p 8888:8888 $(whoami)/$(basename ${PWD})
+# docker run -it --rm -p 8080:8080 -p 8888:8888 $(whoami)/$(basename ${PWD}) /bin/bash
